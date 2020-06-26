f55b096790109cf0ba83a1999f933056269b9129
Alioune <alioune1117@gmail.com>
2020-06-26 21:22:36 +0200

    apportato correzzioni

--- js/script.js
+++ js/script.js
@@ -4,55 +4,58 @@
  // Il numero ottenuto appare al centro del quadrato
 
 
-// EFFETTUO LA CHIAMATA AJAX PER PRENDERE I NUMERI CASUALI DAL SEVER DI BOOLEAN
-
 $(document).ready(function(){
 
-  var chiamataAjax = $.ajax(
 
-    // Inserisco i dati del server da cui fare la chiamata per AJAX
-    {
-      url: "https://flynn.boolean.careers/exercises/api/random/int",
-      method: 'GET',
-      success: function(numero){
-        var numeroCasuale = numero.response;
-        console.log(numeroCasuale);
-
-        // IN CASO DI RICHIESTA ANDATA A BUON FINE:
-        // Creo l'evento per il click su ogni quadrato della griglia
-        $(document).on('click', '.square',
-          function(){
-
-            // Inserisco il numero nel quadrato
-
-            $(this).children('h2').text(numeroCasuale);
-
-            // Se il numero generato è minore o uguale a 5, avrà la lo sfondo giallo
-
-            if (numeroCasuale <= 5) {
-
-              $(this).addClass('yellow');
-
-              // Altrimenti, avrà lo sfondo verde
-            } else {
-
-              $(this).addClass('green');
-
-            }
-            
-          }
-
-        );
-      },
-
-      // IN CASO DI ERRORE:
-      error: function (richiesta, stato, errore){
-        // Visualizzo un messaggio di avviso
-        alert('Qualcosa è andato storto ') + errore;
-      }
-
+    // CREO DINAMICAMENTE LA GRIGLIA 6X6 TRAMITE IL TEMPLATE E UN CICLO FOR
+    for (var i = 0; i < 36; i++) {
+      var source = $('#square_template').html();
+      var template = Handlebars.compile(source);
+      var context = {};
+      var html = template(context);
+      $('.container').append(html);
     }
 
-  );
 
+
+  // EFFETTUO LA CHIAMATA AJAX PER PRENDERE I NUMERI CASUALI DAL SEVER DI BOOLEAN
+
+  // Creo l'evento per il click su ogni quadrato della griglia
+  $(document).on('click', '.square',
+    function(){
+
+      var thisSquare = $(this);
+
+      var chiamataAjax = $.ajax(
+        // Inserisco i dati del server da cui fare la chiamata per AJAX
+        {
+          url: "https://flynn.boolean.careers/exercises/api/random/int",
+          method: 'GET',
+          // IN CASO DI RICHIESTA ANDATA A BUON FINE:
+          success: function(numero){
+            var numeroCasuale = numero.response;
+            console.log(numeroCasuale);
+
+              $(thisSquare).removeClass('yellow');
+              $(thisSquare).removeClass('green');
+
+            // Inserisco il numero nel quadrato
+            thisSquare.children('h2').text(numeroCasuale);
+            // Se il numero generato è minore o uguale a 5, avrà la lo sfondo giallo
+            if (numeroCasuale <= 5) {
+              $(thisSquare).addClass('yellow');
+              // Altrimenti, avrà lo sfondo verde
+            } else {
+              $(thisSquare).addClass('green');
+            }
+          },
+          // IN CASO DI ERRORE:
+          error: function (richiesta, stato, errore){
+            // Visualizzo un messaggio di avviso
+            alert('Qualcosa è andato storto ') + errore;
+          }
+        }
+      );
+    }
+  );
 });
